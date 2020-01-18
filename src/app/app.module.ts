770803bba84ecb26bd3661efcc9c56651b30606f
Gabriel Etzer Dambreville <gabriel-etzer.dambreville@polymtl.ca>
2020-01-18 18:06:04 -0500

    home page update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AgmCoreModule } from "@agm/core";
-import { routes } from "../environments/routes";
-import { environment } from "../environments/environment";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AgmCoreModule } from '@agm/core';
+import { routes } from '../environments/routes';
+import { environment } from '../environments/environment';
 
 //Components
-import { GoogleMapsComponent } from "./google-maps/google-maps.component";
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({

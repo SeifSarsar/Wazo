ffeb268f49e0c3edc5671b0f37c266632caaa358
hichembenm <hibenk@polymtl.ca>
2020-01-19 02:21:10 -0500

    new nav-bar and google maps api

--- src/app/google-maps/google-maps.component.ts
+++ src/app/google-maps/google-maps.component.ts
@@ -6,9 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./google-maps.component.css']
 })
 export class GoogleMapsComponent implements OnInit {
-
+  
   constructor() { }
+  latitude = 45.504384;
+  longitude = -73.6150716;
+  selectedMarker;
+  iconUrl= "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+  markers = [
+    { latitude: 45.51251870000001, longitude: -73.6133858, alpha: 1,icon:{url:this.iconUrl}},
+    { latitude: 45.75606, longitude: -73.5649177, alpha: 1,icon:{url:this.iconUrl}},
+    { latitude: 45.510897, longitude: -73.5649177, alpha: 1 ,icon:{url:this.iconUrl}}
+  ];
 
+
+  selectMarker(event) {
+    this.selectedMarker = {
+      latitude: event.latitude,
+      longitude: event.longitude
+    };
+  }
   ngOnInit() {
   }
 

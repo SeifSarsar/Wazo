94b797e59881a0d72e48b84713d9cc1c5853453a
Gabriel Etzer Dambreville <gabriel-etzer.dambreville@polymtl.ca>
2020-01-19 04:58:42 -0500

    Donation displayed from de database

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Donation } from '../../environments/donation';
 import { Individual } from 'src/environments/individual';
 import { Category, Coordinates } from 'src/environments/global';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home-page',
@@ -10,13 +11,42 @@ import { Category, Coordinates } from 'src/environments/global';
 })
 export class HomePageComponent implements OnInit {
   donations: Donation[] = new Array<Donation>();
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit() {
-    let date: Date = new Date("2019-01-16")
-    let user: Individual = new Individual("15271", "gedambreville@gmail.com", "Etzerthe3rd", 5);
-    let donation: Donation = new Donation("1234", "Beats Pro", user, 3, [], date, Category.Furniture, "beatsProimage.png",new Coordinates(45, 36));
-    this.donations.push(donation);
+   /* let data = new Blob();
+    let arrayOfBlob = new Array<Blob>();
+    arrayOfBlob.push(data);
+    let file: File = new File(arrayOfBlob,'../../assets/SpongeBob-SquarePants-x-Nike-Kyrie-5-Pink.jpeg');
+    let reader: FileReader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      console.log(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };*/
+
+    this.firestore.collection('Donation').get().subscribe(
+      res => {
+        if(!res) throw "Could not find documents";
+        res.forEach((doc)=>{
+          let donation: any = doc.data();
+          let newDonation = new Donation(
+            donation.id,
+            donation.title,
+            donation.userId,
+            donation.capacity,
+            donation.participants,
+            donation.date,
+            donation.category,
+            donation.description,
+            donation.coordinates
+          );
+          this.donations.push(newDonation);
+        })
+      }
+    )
   }
 
 }

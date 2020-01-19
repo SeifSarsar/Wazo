cd567a753508907218472d7ac0c503f7a334ceb6
Seif Sarsar <sesarb@polymtl.ca>
2020-01-19 03:43:53 -0500

    YO

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { StateService } from "../state.service";
-
+import { User } from "../../environments/user";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent {
   constructor(private stateService: StateService) {}
-  navbarOpen = false;
-  toggleSideBar(){
+  private navbarOpen: boolean = false;
+  private username: string;
+  ngOnInit() {
+    this.stateService.userObs.subscribe(
+      user => {
+        if (user) {
+          this.username = user.getUsername();
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  toggleSideBar() {
     this.stateService.toggleSideBar();
   }
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
-
 }
